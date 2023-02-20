eafa50e2a847003823de03974c9db581ad45f06d
Matthew Benjamin <mbenjamin618@gmail.com>
2023-02-19 18:05:27 -0800

    initialize with Chakra and TypeScript

---
+++ components/layout/NavBar.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import Link from 'next/link'
+
+const NavBar = () => {
+ return <div></div>
+}
+
+export default NavBar
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { Lato } from '@next/font/google'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Layout } from '../components'
+import '../styles/globals.css'
+
+const lato = Lato({ subsets: ['latin'], weight: '400' })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Layout>
+        <main className={lato.className}>
+          <Component {...pageProps} />
+        </main>
+      </Layout>
+    </ChakraProvider>
+  )
 }
